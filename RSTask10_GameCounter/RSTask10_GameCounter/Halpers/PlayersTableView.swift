f3d6c2ddd07fb872077ff9227ece9f53b8d8fdfa
VyakulinSA <VyakulinSA@gmail.com>
2021-08-26 10:32:39 +0400

    new restart

---
+++ RSTask10_GameCounter/RSTask10_GameCounter/Halpers/PlayersTableView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayersTableView.swift
+//  RSTask10_GameCounter
+//
+//  Created by Вякулин Сергей on 25.08.2021.
+//
+
+import UIKit
+
+class PlayersTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
